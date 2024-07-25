231f1770c79de6f58d1759cb473db3fa8de24857
Danny Kopping <danny@coder.com>
2024-07-25 22:00:50 +0200

    Update template method API
    Signed-off-by: Danny Kopping <danny@coder.com>

--- site/src/api/typesGenerated.ts
+++ site/src/api/typesGenerated.ts
@@ -1436,6 +1436,11 @@ export interface UpdateCheckResponse {
   readonly url: string;
 }
 
+// From codersdk/notifications.go
+export interface UpdateNotificationTemplateMethod {
+  readonly method?: string;
+}
+
 // From codersdk/organizations.go
 export interface UpdateOrganizationRequest {
   readonly name?: string;
